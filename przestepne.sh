e6d620d94c738968515444a1a246f14e08116961
natalia <ul0306010@edu.uni.lodz.pl>
2025-11-25 21:19:47 +0100

    conditions added

--- przestepne.sh
+++ przestepne.sh
@@ -3,4 +3,9 @@
 =======
 read -p "podaj rok: " year
 
+if [[ (year%4 -eq 0 && year%100 -ne 0) || (year%400 -eq 0) ]]; then
+	echo "$year jest przestepny"
+else
+	echo "$year nie jest przestepny"
+fi
 >>>>>>> bc339cdc626c4da603c45e37908f6e95dc4c96cd
